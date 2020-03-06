a77d9821c8feed7b8bfa818dbf7b48fb1e946ab1
shafali03 <shaf03sm@gmail.com>
2020-03-06 10:09:26 +0000

    testing array with 3 values

--- Unit_test/chunk.test.js
+++ Unit_test/chunk.test.js
@@ -13,3 +13,13 @@ test('Chunk an array of values with length of 2', () => {
   expect(chunkedArr).toEqual([[1,2], [3,4], [5,6], [7,8], [9,10]])
 });
 
+
+test('Chunk an array of 3 values with length of 1', () => {
+  const numbers = [1, 2, 3];
+  const len = 1;
+  const chunkedArr = chunkArray(numbers, len);
+
+
+  expect(chunkedArr).toEqual([[1], [2], [3]])
+});
+
