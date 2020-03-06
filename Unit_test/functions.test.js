cabab326f019c2a0cbfb3398def3c89c01cbb0ab
shafali03 <shaf03sm@gmail.com>
2020-03-06 09:14:29 +0000

    test passed for fetching user name

--- Unit_test/functions.test.js
+++ Unit_test/functions.test.js
@@ -37,6 +37,17 @@ test('There is no I in team',() => {
 })
 
 test('should be a username', () => {
-  usernames = ['john', 'karen'];
-  expect(username).toContain('admin')
+  usernames = ['john', 'karen', 'admin'];
+  expect(usernames).toContain('admin')
 }) 
+
+
+// working with async data
+
+test('User fetched name should be Leanne Graham', () => {
+  expect.assertions(1);
+  return functions.fetchUser()
+  .then(data => {
+    expect(data.name).toEqual('Leanne Graham');
+  })
+})
