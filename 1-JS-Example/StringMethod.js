d75d0d512b3e3169b3e89f62eef780e79a5d9535
shafali03 <shaf03sm@gmail.com>
2020-03-11 11:42:13 +0000

    Strings methods

---
+++ 1-JS-Example/StringMethod.js
@@ -0,0 +1,27 @@
+const message = "Hi what time is it"
+
+const stringObject = new String("Bye")
+
+// String primitive javascript coverts the string into an object then back into a primitive in the background.
+
+
+// None of these modify the original string but return a new string
+
+
+
+message.includes('H'); // true
+
+message.slice("7") // Hi what
+
+message.replace("Hi", "Hey"); // Hey
+
+message.match(/[A-Z]/g); // with match method we can return an all capital letters
+
+
+const message = "Hi what time is it"
+
+message.split('');
+// ['Hi', 'what', 'time', 'is', 'it']
+
+
+
